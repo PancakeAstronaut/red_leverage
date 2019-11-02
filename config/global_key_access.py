97e647f22c415e600796c85ec6fd5ff7041ac9d7
PancakeAstronaut <senormrsir@gmail.com>
2019-11-02 16:21:09 -0400

    added the global key file, it holds all the api keys for use throughout the backend

---
+++ config/global_key_access.py
@@ -0,0 +1,6 @@
+
+# place API Keys here and refer to them to use
+# make sure you include this file in the backend files that use maps
+MAPS_API_KEY = "AIzaSyCIsxAwUtEZFrn1-4TY-ahTVzs5SWMPv6w"    # DON'T SHARE THIS WITH ANYONE
+
+
