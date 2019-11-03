4a6039286119faf5846c3bd5375573f3b3fb142e
PancakeAstronaut <senormrsir@gmail.com>
2019-11-02 20:15:28 -0400

    Added the Accuweather API Key to Key file

--- config/global_key_access.py
+++ config/global_key_access.py
@@ -1,6 +1,8 @@
 
 # place API Keys here and refer to them to use
 # make sure you include this file in the backend files that use maps
+# Google Maps API Key
 MAPS_API_KEY = "AIzaSyCIsxAwUtEZFrn1-4TY-ahTVzs5SWMPv6w"    # DON'T SHARE THIS WITH ANYONE
-
+# Accuweather API Key
+WEATHER_API_KEY = "dSOUgbc9ggTTbx2CqIJR8DkWCcS9WWX8"
 
