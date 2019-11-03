067e951be9709e250a1eee360d1d07bca71459bc
PancakeAstronaut <senormrsir@gmail.com>
2019-11-03 11:13:14 -0500

    added Account creation

--- backend/dbConn.py
+++ backend/dbConn.py
@@ -5,11 +5,27 @@ def arb():
     print("Not supposed to see this")
 
 
+def sql_injection(username, password, email):
+    dbconnection = sqlite3.connect('login.db')
+    sqlite_cursor = dbconnection.cursor()
+    sqlite_cursor.execute("INSERT INTO LoginInfo VALUES (?, ?, ?)", (username, password, email))
+    dbconnection.commit()
+    sqlite_cursor.execute("SELECT UserName, UserPassword FROM LoginInfo WHERE UserName = (?) AND UserPassword = (?)",
+                          (username, password,))
+    resultList = sqlite_cursor.fetchall()
+    dbconnection.close()
+    if len(resultList) <= 0:
+        return 0
+    else:
+        return 1
+
+
 def credentials_validator(username, password):
     dbconnection = sqlite3.connect('login.db')
     sqlite_cursor = dbconnection.cursor()
     sqlite_cursor.execute("SELECT UserName, UserPassword FROM LoginInfo WHERE UserName = (?) AND UserPassword = (?)", (username, password, ))
     resultList = sqlite_cursor.fetchall()
+    dbconnection.close()
     if len(resultList) <= 0:
         return [0, 0]
     else:
